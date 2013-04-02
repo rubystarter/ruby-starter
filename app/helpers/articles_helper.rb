80abf6423f1fdc4ce98fd89e8df2ab1571305b42
LEX LIU <lex.liu@hotmail.com>
2013-04-01 23:36:09 -0700

    welcome name

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,8 +1,12 @@
 module ArticlesHelper
   def get_user_name(id)
-    article_user = User.where("id = ?", id)
-    if (article_user.length>0)
-      article_user.first.name
+    find_user = User.where("id = ?", id)
+    if (find_user.length>0)
+      if find_user.first.name.nil?
+      	""
+      else
+      	find_user.first.name
+      end
     else
       ""
     end
